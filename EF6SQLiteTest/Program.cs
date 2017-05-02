b5a6eee5527c9c0d4b26e6c4311c4a9be7f4a9ae
Mark Antill <mark.antill@countercept.com>
2017-05-02 12:20:55 +0100

    first commit

---
+++ EF6SQLiteTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6SQLiteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MyContext("Data Source = mytest.db; Version = 3;");
+            var entities = context.MyEntities.ToList();
+            context.MyEntities.Add(new MyEntity {Id = 10, Stuff = "I exist "});
+            context.SaveChanges();
+        }
+    }
+}
